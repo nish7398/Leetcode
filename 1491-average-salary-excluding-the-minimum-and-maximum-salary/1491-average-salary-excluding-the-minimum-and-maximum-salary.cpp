1c690fecf5db806df4d106310a17c46541d7ecfb
nish7398 <85886191+nish7398@users.noreply.github.com>
2023-05-01 21:14:15 +0530

    Time: 5 ms (23.02%), Space: 7.1 MB (53.11%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,26 +1,24 @@
+// Approach:- 
+
+// Sort the array of salaries in ascending order.
+// Compute the sum of salaries by iterating over the sorted array.
+// Compute the sum of salaries excluding the minimum and maximum values (i.e., the first and last elements of the sorted array).
+// Divide the sum of salaries excluding the minimum and maximum values by the count of salaries minus two to obtain the average salary.
+// Return the average salary.
+
+
 class Solution {
 public:
     double average(vector<int>& salary) {
-        if(salary.size() <=2 ) 
-            return 0;
+        sort (salary.begin() , salary.end());
         
-        int min =salary[0] > salary[1] ? salary[0]:salary[1];
-        int max=salary[0]>salary[1]?salary[1]:salary[0];
+        double sum=0;
         
-        double res=0;
-         for(int i=0;i<salary.size();i++){
-             if(min > salary[i]){
-                 min=salary[i];
-             }
-             if(max<salary[i]){
-                 max=salary[i];
-             }
-             
-             res = res + salary[i];
-         }
-        res=res - min;
-        res = res - max;
+        for(int i=0;i<salary.size();i++){
+            sum +=salary[i];
+        }
         
-        return res/(salary.size()-2);
+        return (sum - salary[0] -salary[salary.size()-1]) / (salary.size() - 2);
+        
     }
 };
